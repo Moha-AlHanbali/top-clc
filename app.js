08dbd3f79f7a8f4221c0c06d9b39f8f6b607b098
Moha-AlHanbali <moha.alhanbali@gmail.com>
2023-09-10 16:42:26 +0300

    feat:  registering operator and operand button inputs

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 "use strict";
 
+let leftOperand = "";
+let rightOperand = "";
+let operator = "";
+let display = "";
+
+const operationList = ["+", "-", "*", "/"];
+const screenDisplay = document.getElementsByClassName('calculator-screen')[0];
+const numbers = Array.from(document.getElementsByClassName('number-button'));
+const operators = Array.from(document.getElementsByClassName('function-button'));
+
 const add = (x, y) => { return x + y };
 const subtract = (x, y) => { return x - y };
 const multiply = (x, y) => { return x * y };
 const divide = (x, y) => { return x / y };
 
 const operate = (operation, x, y) => {
+    x = Number(x);
+    y = Number(y);
     switch (operation) {
         case "+":
             return add(x, y);
@@ -22,10 +34,53 @@ const operate = (operation, x, y) => {
         default:
             return
     };
+
+};
+
+const resetValues = () => {
+    leftOperand = "";
+    rightOperand = "";
+    operator = "";
+    display = "";
+    screenDisplay.textContent = "0";
+    return
 }
 
-let leftOperand = "";
-let rightOperand = "";
-let operator = "";
-let display = "";
 
+numbers.forEach(numberButton => {
+    numberButton.addEventListener('click', (e) => {
+        registerInput(e.target.value);
+    });
+});
+
+operators.forEach(numberButton => {
+    numberButton.addEventListener('click', (e) => {
+        registerInput(e.target.value);
+    });
+});
+
+const registerInput = (buttonValue) => {
+    if (operationList.includes(buttonValue)) {
+        if (rightOperand) {
+            leftOperand = operate(operator, leftOperand, rightOperand);
+            rightOperand = "";
+        }
+        operator = buttonValue;
+        display = `${leftOperand} ${operator} ${rightOperand}`
+        screenDisplay.textContent = display;
+        return
+    }
+    try {
+        const numericValue = parseFloat(buttonValue);
+        if (!operator) leftOperand += numericValue;
+        else rightOperand += numericValue;
+        display = `${leftOperand} ${operator} ${rightOperand}`
+        screenDisplay.textContent = display;
+    }
+    catch {
+
+    }
+    return
+}
+
+resetValues();
