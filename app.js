284921882c7563148b31576c7adf6a880392cb2f
Moha-AlHanbali <moha.alhanbali@gmail.com>
2023-09-10 13:50:40 +0300

    feat: added base arithmetic functions

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const add = (x, y) => { return x + y };
+const subtract = (x, y) => { return x - y };
+const multiply = (x, y) => { return x * y };
+const divide = (x, y) => { return x / y };
+
+const operate = (operation, x, y) => {
+    switch (operation) {
+        case "+":
+            return add(x, y);
+
+        case "-":
+            return subtract(x, y);
+
+        case "*":
+            return multiply(x, y);
+
+        case "/":
+            return divide(x, y);
+
+        default:
+            return
+    };
+}
+
+let leftOperand = "";
+let rightOperand = "";
+let operator = "";
+let display = "";
+
